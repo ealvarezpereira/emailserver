11308bd225168ba657494ec84590c302f11d7a81
Enrique Alvarez Pereira <quique.alvarezp@gmail.com>
2026-03-03 21:59:55 +0100

    Initial commit

---
+++ email-manager/src/test/java/com/gbtec/emailmanager/EmailManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gbtec.emailmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EmailManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
